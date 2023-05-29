33963524b5dff776f3c41a01bcd12679f7e271d9
shizokura <edwardguevarra2003@gmail.com>
2023-05-30 05:55:04 +0800

    Dynamic Year

--- app/components/Home/About.tsx
+++ app/components/Home/About.tsx
@@ -7,10 +7,10 @@ const About = () => (
       <div className={ aboutStyle.intro }>
         <div className={ aboutStyle.text }>
           <div className={ aboutStyle.text__title }>Hi, I'm <strong>Edward Guevarra</strong></div>
-          <div className={ aboutStyle.text__description }>{`I'm a web developer with experience since 2016, specializing in PHP, JavaScript, NodeJS, HTML, CSS, and modern frameworks like React, Vue, and Angular. I have a strong background in backend development, managing server infrastructure, and creating efficient APIs using Node.js. My expertise extends to databases, including MySQL, MongoDB, and Firestore. I'm constantly eager to learn and improve my skills to deliver exceptional solutions. Let's work together to bring your ideas to life!`}</div>
+          <div className={ aboutStyle.text__description }>{`I'm a web developer with experience since 2015, specializing in PHP, JavaScript, NodeJS, HTML, CSS, and modern frameworks like React, Vue, and Angular. I have a strong background in backend development, managing server infrastructure, and creating efficient APIs using Node.js. My expertise extends to databases, including MySQL, MongoDB, and Firestore. I'm constantly eager to learn and improve my skills to deliver exceptional solutions. Let's work together to bring your ideas to life!`}</div>
         </div>
         <div className={ aboutStyle.years }>
-          <div className={ aboutStyle.years__number }>8</div>
+          <div className={ aboutStyle.years__number }>{ new Date().getFullYear() - 2015 }</div>
           <div className={ aboutStyle.years__text }>Years of <strong>Experience</strong></div>
         </div>
       </div>
